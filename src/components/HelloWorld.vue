9edcf0e63cb3ce08f1a855955cbe19d4a206b37d
Kyle Humphrey <kuenaimaku@gmail.com>
2019-04-16 22:12:45 -0500

    Initial Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+    <div class="columns">
+      <div class="column">
+        <b-table
+            :data="isEmpty ? [] : data"
+            :bordered="isBordered"
+            :striped="isStriped"
+            :narrowed="isNarrowed"
+            :hoverable="isHoverable"
+            :loading="isLoading"
+            :focusable="isFocusable"
+            :mobile-cards="hasMobileCards">
+
+            <template slot-scope="props">
+                <b-table-column field="id" label="Order" width="40" numeric>
+                    {{ props.row.id }}
+                </b-table-column>
+
+                <b-table-column field="summoner_name" label="Summoner">
+                    {{ props.row.summoner_name }}
+                </b-table-column>
+
+                <b-table-column field="common_name" label="Name">
+                    {{ props.row.common_name }}
+                </b-table-column>
+
+                <b-table-column field="role" label="Role" centered>
+                    <span class="tag is-success">
+                        {{ props.row.role }}
+                    </span>
+                </b-table-column>
+            </template>
+
+            <template slot="empty">
+                <section class="section">
+                    <div class="content has-text-grey has-text-centered">
+                        <p>
+                            <b-icon
+                                icon="emoticon-sad"
+                                size="is-large">
+                            </b-icon>
+                        </p>
+                        <p>Nothing here.</p>
+                    </div>
+                </section>
+            </template>
+        </b-table>
+      </div>
+      <div class="column">
+        <b-table
+            :data="isEmpty ? [] : data"
+            :bordered="isBordered"
+            :striped="isStriped"
+            :narrowed="isNarrowed"
+            :hoverable="isHoverable"
+            :loading="isLoading"
+            :focusable="isFocusable"
+            :mobile-cards="hasMobileCards">
+
+            <template slot-scope="props">
+                <b-table-column field="id" label="ID" width="40" numeric>
+                    {{ props.row.id }}
+                </b-table-column>
+
+                <b-table-column field="summoner_name" label="Summoner">
+                    {{ props.row.summoner_name }}
+                </b-table-column>
+
+                <b-table-column field="last_name" label="Name">
+                    {{ props.row.common_name }}
+                </b-table-column>
+
+                <b-table-column field="role" label="Role" centered>
+                    <span class="tag is-success">
+                        {{ props.row.role }}
+                    </span>
+                </b-table-column>
+            </template>
+
+            <template slot="empty">
+                <section class="section">
+                    <div class="content has-text-grey has-text-centered">
+                        <p>
+                            <b-icon
+                                icon="emoticon-sad"
+                                size="is-large">
+                            </b-icon>
+                        </p>
+                        <p>Nothing here.</p>
+                    </div>
+                </section>
+            </template>
+        </b-table>
+      </div>
+    </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+<script>
+    export default {
+        data() {
+            const data = [
+                { 'id': 1, 'summoner_name': 'Kuenaimaku', 'common_name': 'Kyle', 'role': 'Support' },
+                { 'id': 2, 'summoner_name': 'Peruman', 'common_name': 'Peru', 'role': 'Jungle' },
+                { 'id': 3, 'summoner_name': 'ArmoredKoko47', 'common_name': 'Eric', 'role': 'Bot' },
+                { 'id': 4, 'summoner_name': 'Pudash', 'common_name': 'Jason', 'role': 'Top' },
+                { 'id': 5, 'summoner_name': 'GlacialPuppy', 'common_name': 'Sean', 'role': 'Mid' },
+            ]
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+            return {
+                data,
+                isEmpty: false,
+                isBordered: false,
+                isStriped: false,
+                isNarrowed: false,
+                isHoverable: false,
+                isFocusable: false,
+                isLoading: false,
+                hasMobileCards: true
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy, {defaultIconPack: 'fas'})
+
 Vue.config.productionTip = false;
 
 new Vue({
